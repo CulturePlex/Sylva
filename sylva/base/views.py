0edbd6a91ab6126e31c8dd8db1298dfd9e2de8a7
Javier de la Rosa <versae@gmail.com>
2011-06-24 14:53:24 -0400

    Adding support for signing up only for superusers.

--- sylva/base/views.py
+++ sylva/base/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, redirect
 from django.template import RequestContext
 
+from userena.views import signup
+
 
 def index(request):
     if request.user.is_authenticated():
@@ -10,6 +12,11 @@ def index(request):
     return render_to_response('index.html',
                               {'is_index': True},
                               context_instance=RequestContext(request))
+
+def signup_redirect(request, *args, **kwargs):
+    if request.user.is_authenticated() and request.user.is_superuser:
+        return signup(request, *args, **kwargs)
+    return redirect("index")
 
 @login_required()
 def dashboard(request):
