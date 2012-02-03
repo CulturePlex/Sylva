b4e0dbbf7f88d7a33ec29e7480c810ee2d323b2b
Javier de la Rosa <versae@gmail.com>
2012-02-03 16:43:55 -0500

    Fixing the database path

--- sylva/settings.py
+++ sylva/settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'sylva.sqlite',          # Or path to database file if using sqlite3.
+        'NAME': path.join(PROJECT_ROOT, 'sylva.sqlite'),          # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
