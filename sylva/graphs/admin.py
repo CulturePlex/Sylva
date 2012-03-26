0f6da9535b8c5de6afefd2160ded1c741422545e
Javier de la Rosa <versae@gmail.com>
2012-03-26 17:50:03 -0400

    Adding admin for object permissions

--- sylva/graphs/admin.py
+++ sylva/graphs/admin.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
+from guardian.admin import GuardedModelAdmin
+
 from graphs.models import Graph
 
 
-admin.site.register(Graph)
+class GraphAdmin(GuardedModelAdmin):
+    user_owned_objects_field = "owner"
+
+admin.site.register(Graph, GraphAdmin)
