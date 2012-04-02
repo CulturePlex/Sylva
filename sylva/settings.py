c66f963fe33d46e590d6f8498ebbdefdedec1f06
Javier de la Rosa <versae@gmail.com>
2012-04-02 17:16:11 -0400

    Adding /media/ and increasing the number of nodes to show in the preview

--- sylva/settings.py
+++ sylva/settings.py
@@ -81,7 +81,7 @@ MEDIA_ROOT = path.join(PROJECT_ROOT, 'media')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -244,4 +244,4 @@ OPTIONS = {
 }
 
 # This setting controls the size of the graph preview in the graph screen
-PREVIEW_NODES = 15
+PREVIEW_NODES = 25
