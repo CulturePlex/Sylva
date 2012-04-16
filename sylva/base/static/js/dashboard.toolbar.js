a49ba3e943e5a0119eed7d86cca98d4a98a44e2d
Javier de la Rosa <versae@gmail.com>
2012-04-16 15:45:35 -0400

    Adding a menu for data browsing

--- sylva/base/static/js/dashboard.toolbar.js
+++ sylva/base/static/js/dashboard.toolbar.js
@@ -4,6 +4,15 @@
             location.href = $(this).val();
             return false;
         });
+        $("#dataMenu").on("mouseover", function(e) {
+            $("#dataBrowse").show();
+        });
+        $(document).on("click", function(event){
+            var target = $(event.target);
+            if (target.parents("#dataBrowse").length == 0) {
+                $("#dataBrowse").hide();
+            }
+        });
     };
     $(document).ready(init);
 })(jQuery);
