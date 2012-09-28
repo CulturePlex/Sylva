91c5de34caeadc943b165620ed30f7fafcb1fe96
Sergio Rus <geiros@gmail.com>
2012-09-28 19:34:05 +0200

    Cloning: add 'clone' button in nav menu
    Move 'export' and 'cloning' links into a new 'Tools' drop-down menu

--- sylva/base/static/js/dashboard.toolbar.js
+++ sylva/base/static/js/dashboard.toolbar.js
@@ -4,16 +4,24 @@
             location.href = $(this).val();
             return false;
         });
-        $("#dataMenu").on("mouseover", function(e) {
+        $("#dataMenu").on("mouseenter", function(e) {
             $("#dataBrowse").show();
+        });
+        $("#dataBrowse").on("mouseleave", function(e) {
+           $(this).hide();
+        });
+        $("#toolsMenu").on("mouseenter", function(e) {
+            $("#toolsBrowseId").show();
+        });
+        $("#toolsBrowseId").on("mouseleave", function(e) {
+           $(this).hide();
         });
         $(document).on("click", function(event){
             var target = $(event.target);
-            if (target.parents("#dataBrowse").length == 0) {
+            if (target.parents("#dataBrowse").length === 0) {
                 $("#dataBrowse").hide();
             }
         });
     };
     $(document).ready(init);
 })(jQuery);
-//})(jQuery.noConflict());
