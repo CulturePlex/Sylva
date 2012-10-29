0dc90452a2b1c013fe26373c26934ce625846bbe
Sergio Rus <geiros@gmail.com>
2012-10-29 18:20:22 +0100

    Add spinner (see trello#45)

--- sylva/graphs/static/graphs/js/visualizations.js
+++ sylva/graphs/static/graphs/js/visualizations.js
@@ -37,8 +37,30 @@ sylv:true, alert:true */
 
   // DOM
   $(function() {
+
+    var opts = {
+      lines: 15,            // The number of lines to draw
+      length: 16,           // The length of each line
+      width: 5,             // The line thickness
+      radius: 13,           // The radius of the inner circle
+      corners: 1,           // Corner roundness (0..1)
+      rotate: 0,            // The rotation offset
+      color: '#000',        // #rgb or #rrggbb
+      speed: 1,             // Rounds per second
+      trail: 60,            // Afterglow percentage
+      shadow: false,         // Whether to render a shadow
+      hwaccel: false,       // Whether to use hardware acceleration
+      className: 'spinner', // The CSS class to assign to the spinner
+      zIndex: 2e9,          // The z-index (defaults to 2000000000)
+      top: '100',           // Top position relative to parent in px
+      left: '650'           // Left position relative to parent in px
+    };
+    var target = document.getElementById('spinner');
+    var spinner = new Spinner(opts).spin(target);
+
     // Graph rendering
     $.getJSON(sylv.ajax_url, function(data) {
+      spinner.stop();
 
       // partial graph (Processing.js)
       sylv.nodes = data.nodes;
