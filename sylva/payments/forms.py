fb04077a2aeeaf8f93483bfc8984157adaec1953
Sergio Rus <geiros@gmail.com>
2013-01-29 01:12:44 +0100

    Payments: Move Stripe logic to the form

--- sylva/payments/forms.py
+++ sylva/payments/forms.py
@@ -1,10 +1,72 @@
+import logging
+
 from django import forms
+from django.template.defaultfilters import slugify
 
-from payments.models import StripeCustomer, StripeSubscription
+from zebra.forms import StripePaymentForm
+
+from payments.models import StripeCustomer, StripeSubscription, StripePlan
+
+import stripe
 
 
-class UnsubscribeForm(forms.Form):
-    pass
+logger = logging.getLogger('payments')
+
+
+class SubscriptionForm(StripePaymentForm):
+
+    def stripe_create_subscription(self, user, plan_name, default_error_message):
+        customer = None
+        stripe_errors = False
+        error_message = default_error_message
+        stripe_token = self.cleaned_data['stripe_token']
+        try:
+            user.stripe_customer
+        except StripeCustomer.DoesNotExist:
+            try:
+                customer = StripeCustomer.objects.create(user=user,
+                                                         card=stripe_token)
+                plan = StripePlan.objects.get(stripe_plan_id=slugify(plan_name))
+                StripeSubscription.objects.create(customer=customer,
+                                                  plan=plan)
+            except stripe.CardError, e:
+                stripe_errors = True
+                error = e.json_body['error']
+                if error['code'] in ['missing', 'processing_error']:
+                    logger.info('payments: %s: (%s) %s' %
+                           (error['type'], error['code'], error['message']))
+                else:
+                    error_message = error['message']
+            except (stripe.InvalidRequestError,
+                    stripe.AuthenticationError,
+                    stripe.APIConnectionError,
+                    stripe.StripeError), e:
+                stripe_errors = True
+                error = e.json_body['error']
+                logger.info('payments: %s: %s' %
+                                    (error['type'], error['message']))
+                if stripe_errors and customer:
+                    customer.delete()
+
+        return stripe_errors, error_message
+
+
+class UnsubscriptionForm(forms.Form):
+
+    def stripe_cancel_subscription(self, customer):
+        stripe_errors = False
+        try:
+            stripe_customer = customer.stripe_customer
+            stripe_customer.cancel_subscription()
+        except (stripe.InvalidRequestError,
+                stripe.AuthenticationError,
+                stripe.APIConnectionError,
+                stripe.StripeError), e:
+            stripe_errors = True
+            error = e.json_body['error']
+            logger.info('payments: %s: %s' % (error['type'], error['message']))
+
+        return stripe_errors
 
 
 class StripeCustomerAdminForm(forms.ModelForm):
--- sylva/payments/views.py
+++ sylva/payments/views.py
@@ -1,28 +1,15 @@
 # -*- encoding: utf-8 -*-
-import logging
-
 from django.conf import settings
 from django.contrib import messages
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
-from django.core.urlresolvers import reverse
-from django.core.exceptions import ObjectDoesNotExist
 from django.template import RequestContext
 from django.shortcuts import redirect
 from django.utils.translation import ugettext_lazy as _
-from django.template.defaultfilters import slugify
 
 from accounts.models import Account
 
-from zebra.forms import StripePaymentForm
-
-from payments.models import StripeCustomer, StripePlan, StripeSubscription
-from payments.forms import UnsubscribeForm
-
-import stripe
-
-
-logger = logging.getLogger('payments')
+from payments.forms import StripeCustomer, SubscriptionForm, UnsubscriptionForm
 
 
 @login_required
@@ -34,56 +21,31 @@ def subscription_create(request, plan_name=''):
     if is_subscribed:
         messages.info(request, _('You are already subscribed for a %s plan.'
                                                             % account_name))
-        return redirect(reverse('dashboard'))
-    customer = None
+        return redirect('dashboard')
     stripe_errors = False
-    error_msg = _('Sorry, an error occurred while processing the card. Your '
-                  'payment could not be processed.')
-    form = StripePaymentForm()
+    error_message = ''
+    default_error_message = _('Sorry, an error occurred while processing the '
+                              'card. Your payment could not be processed.')
+    form = SubscriptionForm()
     if request.method == 'POST':
-        form = StripePaymentForm(request.POST)
+        form = SubscriptionForm(request.POST)
         if form.is_valid():
-            stripe_token = form.cleaned_data['stripe_token']
+            account = None
             try:
-                user.stripe_customer
-            except ObjectDoesNotExist:
-                try:
-                    customer = StripeCustomer.objects.create(user=user,
-                                                             card=stripe_token)
-                    plan = StripePlan.objects.get(stripe_plan_id=slugify(plan_name))
-                    StripeSubscription.objects.create(customer=customer,
-                                                      plan=plan)
-                except stripe.CardError, e:
-                    stripe_errors = True
-                    error = e.json_body['error']
-                    if error['code'] in ['missing', 'processing_error']:
-                        messages.error(request, error_msg)
-                        logger.info('payments: %s: (%s) %s' %
-                            (error['type'], error['code'], error['message']))
-                    else:
-                        messages.error(request, error['message'])
-                except (stripe.InvalidRequestError,
-                        stripe.AuthenticationError,
-                        stripe.APIConnectionError,
-                        stripe.StripeError), e:
-                    stripe_errors = True
-                    error = e.json_body['error']
-                    messages.error(request, error_msg)
-                    logger.info('payments: %s: %s' %
-                                (error['type'], error['message']))
+                account = Account.objects.get(name=plan_name)
+            except Account.DoesNotExist:
+                messages.error(request, default_error_message)
+                return redirect('dashboard')
+            stripe_errors, error_message = \
+                        form.stripe_create_subscription(user, plan_name,
+                                                        default_error_message)
             if stripe_errors:
-                if customer:
-                    customer.delete()
+                messages.error(request, error_message)
             else:
                 profile = user.get_profile()
-                try:
-                    account = Account.objects.get(name=plan_name)
-                except Account.DoesNotExist:
-                    messages.error(request, error_msg)
-                    return redirect(reverse('dashboard'))
                 profile.account = account
                 profile.save()
-                return redirect(reverse('subscription_welcome'))
+                return redirect('subscription_welcome')
     return render_to_response('payments/subscription_create.html',
                               {'form': form,
                                'plan_name': plan_name,
@@ -98,45 +60,35 @@ def subscription_cancel(request):
     account_name = profile.account.name
     is_subscribed = account_name == 'Basic' or account_name == 'Premium'
     if not is_subscribed:
-        return redirect(reverse('dashboard'))
+        return redirect('dashboard')
     customer = None
     stripe_errors = False
-    error_msg = _('Sorry, an error occurred while cancelling your '
-                  'subscription. Please, contact us and we will try to solve '
-                  'your problem as soon as possible.')
+    error_message = _('Sorry, an error occurred while cancelling your '
+                      'subscription. Please, contact us and we will '
+                      'try to solve your problem as soon as possible.')
     try:
         customer = user.stripe_customer
-    except ObjectDoesNotExist:
-        return redirect(reverse('dashboard'))
-    form = UnsubscribeForm()
+    except StripeCustomer.DoesNotExist:
+        return redirect('dashboard')
+    form = UnsubscriptionForm()
     if request.method == 'POST':
-        form = UnsubscribeForm(request.POST)
+        form = UnsubscriptionForm(request.POST)
         if form.is_valid():
             try:
-                stripe_customer = customer.stripe_customer
-                stripe_customer.cancel_subscription()
-            except (stripe.InvalidRequestError,
-                    stripe.AuthenticationError,
-                    stripe.APIConnectionError,
-                    stripe.StripeError), e:
-                stripe_errors = True
-                error = e.json_body['error']
-                logger.info('payments: %s: %s' %
-                            (error['type'], error['message']))
-            if not stripe_errors:
+                account = Account.objects.get(name='Free')
+            except Account.DoesNotExist:
+                messages.error(request, error_message)
+                return redirect('dashboard')
+            stripe_errors = form.stripe_cancel_subscription(customer)
+            if stripe_errors:
+                messages.error(request, error_message)
+            else:
                 profile = user.get_profile()
-                try:
-                    account = Account.objects.get(name='Free')
-                except Account.DoesNotExist:
-                    messages.error(request, error_msg)
-                    return redirect(reverse('dashboard'))
                 profile.account = account
                 profile.save()
                 customer.delete()
                 messages.success(request, 'You have successfully unsubscribed')
-                return redirect(reverse('dashboard'))
-            else:
-                messages.error(request, error_msg)
+                return redirect('dashboard')
     return render_to_response('payments/subscription_cancel.html',
                               {'form': form},
                               context_instance=RequestContext(request))
@@ -149,7 +101,7 @@ def subscription_welcome(request):
     account_name = profile.account.name
     is_subscribed = account_name == 'Basic' or account_name == 'Premium'
     if not is_subscribed:
-        return redirect(reverse('dashboard'))
+        return redirect('dashboard')
     return render_to_response('payments/subscription_welcome.html',
                               {'user': user,
                                'account_name': account_name},
