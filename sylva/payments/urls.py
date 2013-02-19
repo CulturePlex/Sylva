93ea61a08053bd86890c52493a7818d35612ba08
Sergio Rus <geiros@gmail.com>
2013-02-19 16:49:59 +0100

    Payments: Add routes

--- sylva/payments/urls.py
+++ sylva/payments/urls.py
@@ -5,9 +5,9 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('payments.views',
     url(r'^subscribe/basic/$', 'subscription_edit_create',
-        {'plan_id': '2'}, name='subscription_edit_create'),
+        {'plan_id': '2'}, name='subscription_basic'),
     url(r'^subscribe/premium/$', 'subscription_edit_create',
-        {'plan_id': '3'}, name='subscription_edit_create'),
+        {'plan_id': '3'}, name='subscription_premium'),
     url(r'^unsubscribe/$', 'subscription_cancel', name='subscription_cancel'),
     url(r'^welcome/$', 'subscription_welcome', name='subscription_welcome'),
     url(r'^plans/$', TemplateView.as_view(template_name='payments/plans.html'),
