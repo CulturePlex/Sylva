5d721a7dc745b0a36fe3e412a5957bf593381de4
Sergio Rus <geiros@gmail.com>
2013-02-20 23:45:13 +0100

    Payments: update Welcome page

--- sylva/payments/urls.py
+++ sylva/payments/urls.py
@@ -1,7 +1,5 @@
 # -*- encoding: utf-8 -*-
 from django.conf.urls.defaults import patterns, url
-from django.views.generic import TemplateView
-
 
 urlpatterns = patterns('payments.views',
     url(r'^subscribe/basic/$', 'subscription_edit_create',
@@ -10,6 +8,5 @@ urlpatterns = patterns('payments.views',
         {'plan_id': '3'}, name='subscription_premium'),
     url(r'^unsubscribe/$', 'subscription_cancel', name='subscription_cancel'),
     url(r'^welcome/$', 'subscription_welcome', name='subscription_welcome'),
-    url(r'^plans/$', TemplateView.as_view(template_name='payments/plans.html'),
-        name='subscription_plans'),
+    url(r'^plans/$', 'subscription_plans', name='subscription_plans'),
 )
