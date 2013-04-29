bb393bab9dac9bd82416dfc79f96763406ec1156
Sergio Rus <geiros@gmail.com>
2013-04-29 19:37:15 +0200

    Remove old queries system

--- sylva/operators/urls.py
+++ sylva/operators/urls.py
@@ -17,9 +17,4 @@ urlpatterns = patterns(
         name="operator_query"),
     url(r'^(?P<graph_slug>[\w-]+)/query/results/$', 'operator_query_results',
         name="operator_query_results"),
-
-    url(r'^(?P<graph_slug>[\w-]+)/graph-query/$', 'graph_query',
-        name="graph_query"),
-    url(r'^process-ajax-query/$', 'process_ajax_query',
-        name="process_ajax_query"),
 )
--- sylva/operators/views.py
+++ sylva/operators/views.py
@@ -39,43 +39,6 @@ def operator_builder(request, graph_slug):
 @login_required
 @permission_required("data.view_data", (Graph, "slug", "graph_slug"),
                      return_403=True)
-def graph_query(request, graph_slug):
-    graph = get_object_or_404(Graph, slug=graph_slug)
-    node_types = graph.schema.nodetype_set.all()
-    rel_types = graph.schema.relationshiptype_set.all()
-    node_type_names = [nt.name for nt in node_types]
-    rel_type_names = [rt.name for rt in rel_types]
-    node_properties = []
-    for node_type in node_types:
-        for node_prop in node_type.properties.all():
-            node_properties.append({
-                'value': node_prop.key,
-                'label': '(' + node_type.name + ') ' + node_prop.key
-            })
-    return render_to_response('operators/graph_query.html', {
-        "graph": graph,
-        "node_types": json.dumps(node_type_names),
-        "relationship_types": json.dumps(rel_type_names),
-        "node_properties": json.dumps(node_properties)
-    }, context_instance=RequestContext(request))
-
-
-@is_enabled(settings.ENABLE_QUERIES)
-@login_required
-def process_ajax_query(request):
-    if request.is_ajax() and request.method == 'POST':
-        query = request.POST.get('query')
-        # data = parse_query(query)
-        data = query
-        return HttpResponse(json.dumps(data),
-                            status=200,
-                            mimetype='application/json')
-
-
-@is_enabled(settings.ENABLE_QUERIES)
-@login_required
-@permission_required("data.view_data", (Graph, "slug", "graph_slug"),
-                     return_403=True)
 def operator_query(request, graph_slug):
     graph = get_object_or_404(Graph, slug=graph_slug)
     return render_to_response('operators/operator_query.html',
