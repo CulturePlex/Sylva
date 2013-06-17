71769fb42ca1624cee8b46c018fe926c2a5de9ad
Sergio Rus <geiros@gmail.com>
2013-06-17 20:08:31 +0200

    Data import: ensure that is an AJAX request

--- sylva/tools/views.py
+++ sylva/tools/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import (get_object_or_404, render_to_response,
                               HttpResponse, redirect)
+from django.http import Http404
 from django.utils.translation import gettext as _
 from django.template import RequestContext
 
@@ -41,28 +42,32 @@ def graph_import_tool(request, graph_slug):
 @permission_required("data.change_data", (Data, "graph__slug", "graph_slug"),
                      return_403=True)
 def ajax_node_create(request, graph_slug):
-    graph = get_object_or_404(Graph, slug=graph_slug)
-    data = request.POST
-    label = graph.schema.nodetype_set.get(name=data["type"])
-    properties = json.loads(data.get("properties", "{}"))
-    node = graph.nodes.create(str(label.id), properties)
-    return HttpResponse(json.dumps({"id": node.id}),
-                        mimetype='application/json')
+    if request.is_ajax():
+        graph = get_object_or_404(Graph, slug=graph_slug)
+        data = request.POST
+        label = graph.schema.nodetype_set.get(name=data["type"])
+        properties = json.loads(data.get("properties", "{}"))
+        node = graph.nodes.create(str(label.id), properties)
+        return HttpResponse(json.dumps({"id": node.id}),
+                            mimetype='application/json')
+    raise Http404(_("Error: Invalid request (expected an AJAX request)"))
 
 
 @permission_required("data.change_data", (Data, "graph__slug", "graph_slug"),
                      return_403=True)
 def ajax_relationship_create(request, graph_slug):
-    graph = get_object_or_404(Graph, slug=graph_slug)
-    data = request.POST
-    source = graph.nodes.get(data["sourceId"])
-    target = graph.nodes.get(data["targetId"])
-    label = graph.schema.relationshiptype_set.get(name=data["type"],
-                                                  source=source.label,
-                                                  target=target.label)
-    properties = json.loads(data.get("properties", "{}"))
-    graph.relationships.create(source, target, str(label.id), properties)
-    return HttpResponse(json.dumps({}), mimetype='application/json')
+    if request.is_ajax():
+        graph = get_object_or_404(Graph, slug=graph_slug)
+        data = request.POST
+        source = graph.nodes.get(data["sourceId"])
+        target = graph.nodes.get(data["targetId"])
+        label = graph.schema.relationshiptype_set.get(name=data["type"],
+                                                      source=source.label,
+                                                      target=target.label)
+        properties = json.loads(data.get("properties", "{}"))
+        graph.relationships.create(source, target, str(label.id), properties)
+        return HttpResponse(json.dumps({}), mimetype='application/json')
+    raise Http404(_("Error: Invalid request (expected an AJAX request)"))
 
 
 @permission_required("data.view_data", (Data, "graph__slug", "graph_slug"),
