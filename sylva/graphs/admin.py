02f6c4f6b72bd65ec3f09ebeed5e188cc1c2078d
Sergio Rus <geiros@gmail.com>
2013-07-19 00:45:47 +0200

    Admin: fix 'delete' action for graphs

--- sylva/graphs/admin.py
+++ sylva/graphs/admin.py
@@ -5,8 +5,20 @@ from guardian.admin import GuardedModelAdmin
 
 from graphs.models import Graph
 
+# Globally disable default 'delete selected'
+admin.site.disable_action('delete_selected')
+
+
+def delete_graph(modeladmin, request, queryset):
+    for graph in queryset:
+        graph.relationships.delete()
+        graph.nodes.delete()
+        graph.delete()
+delete_graph.short_description = "Delete selected graphs and related elements"
+
 
 class GraphAdmin(GuardedModelAdmin):
     user_owned_objects_field = "owner"
+    actions = [delete_graph]
 
 admin.site.register(Graph, GraphAdmin)
