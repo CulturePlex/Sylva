0ce5253607e4690605f51a74ebfcb020c1d9f692
gabmunrio <munozrios@gmail.com>
2013-11-13 13:59:48 +0100

    Testing script for Travis with neo4j installation and execution. Testing with the console option

--- download_and_install_neo4j.sh
+++ download_and_install_neo4j.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wget dist.neo4j.org/neo4j-community-1.9.2-unix.tar.gz
-tar -zxvf neo4j-community-1.9.2-unix.tar.gz
+tar -zxvf neo4j-community-1.9.2-unix.tar.gz > /dev/null
 mv neo4j-community-1.9.2 neo4j
 cp ./server.properties neo4j/conf/
 rm -rf neo4j/conf/neo4j-server.properties
