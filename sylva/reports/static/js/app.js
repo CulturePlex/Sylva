131bb9cc6bead934c5369372962dc8072415d3b2
davidmichaelbrown <davidmichaelbrown1@gmail.com>
2014-01-29 23:05:07 -0500

    added report creation (basic)

--- sylva/reports/static/js/app.js
+++ sylva/reports/static/js/app.js
@@ -1,6 +1,25 @@
-var reports = angular.module('reports', ['reportsControllers', 'reportsServices', 'reportsDirectives', 'ngDragDrop']);
+var reports = angular.module('reports', [
+	'reportsControllers',
+	'reportsServices',
+	'reportsDirectives', 
+	'ngCookies'
+]);
 
-reports.config(function($interpolateProvider) {
-  $interpolateProvider.startSymbol('{[{');
-  $interpolateProvider.endSymbol('}]}');
-});
+
+reports.config([
+    '$httpProvider', 
+    '$interpolateProvider', 
+    function($httpProvider, $interpolateProvider) {
+        $interpolateProvider.startSymbol('{$');
+        $interpolateProvider.endSymbol('$}');
+        $httpProvider.defaults.headers.post['Content-Type'] = 
+        							'application/x-www-form-urlencoded';
+    	$httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+    }]).
+    run([
+    '$http', 
+    '$cookies', 
+    function($http, $cookies) {
+        $http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
+    }]);
--- sylva/reports/static/js/controllers.js
+++ sylva/reports/static/js/controllers.js
@@ -1,43 +1,54 @@
-var reportsControllers = angular.module('reportsControllers', ['ngSanitize']);
+var reportsControllers = angular.module('reportsControllers', []);
 
 
-reportsControllers.controller('reportsListCtrl', ['$scope', function($scope) {
+reportsControllers.controller('reportsListCtrl', ['$scope', '$timeout', function($scope, $timeout) {
 	$scope.reports = [
-		{name: 'report1'},
-		{name: 'report2'},
-		{name: 'report3'},
-		{name:'report4'},
-		{name: 'report5'},
-		{name: 'report6'},
-		{name: 'report7'},
-		{name: 'report8'},
-		{name:'report9'},
-		{name: 'report10'},
-		{name: 'report11'},
-		{name: 'report12'},
-		{name: 'report13'},
-		{name:'report14'},
-		{name: 'report15'},
+		{name: 'report1', queries: ['query1', 'query3']},
+		{name: 'report2', queries: ['query5', 'query2']},
+		{name: 'report3', queries: ['query4', 'query5']},
 	];
 
 	$scope.queries = [{name: 'query1'}, {name: 'query2'}, {name: 'query3'}, {name: 'query4'}, {name: 'query5'}]
 
+	$scope.dropped = []
+	$scope.namePlaceholder = 'Report Name'
 	//$scope.reportForm = true;
 	console.log($scope.reportForm)
 	$scope.newReport = function () {
 		console.log('click')
 		$scope.reportForm = true;
-	}
-
-	$scope.dropped = []
+	};	
 
 	$scope.removeQuery = function (index) {
 		console.log(index)
 		if (index > -1) $scope.dropped.splice(index, 1)
-	}
+	};
 
 	$scope.handleDrop = function (drop) {
 		$scope.dropped.push(drop);
 		console.log($scope.dropped)
-	} 
+	}; 
+
+	$scope.processForm = function (reportName) {
+		var report = $scope.reports.filter(function (element) {
+			return element.name === reportSlug;
+		});
+		var newReport = {
+			name: reportName.name,
+			queries: $scope.dropped
+		};
+		$scope.reports.push(newReport)
+		$scope.dropped = []
+		console.log('new', $scope.reports)
+	};
+
+	$scope.getReport = function (reportSlug) {
+		// need to put an edit feature here
+		var report = $scope.reports.filter(function (element) {
+			return element.name === reportSlug;
+		});
+		console.log('applying', report[0].queries)
+		$scope.dropped = report[0].queries
+		$scope.namePlaceholder = report[0].name
+	};
 }]);
