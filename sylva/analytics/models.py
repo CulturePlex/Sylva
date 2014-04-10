74d6bad3f36cf0ad3ff68959b9380be9817f9c13
gabmunrio <munozrios22@gmail.com>
2014-04-10 12:19:50 -0400

    More improvements in the models and files related to the analytics. We are testing all the changes little by little to see that all work correctly.

--- sylva/analytics/models.py
+++ sylva/analytics/models.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-import datetime
 
 from django.utils.translation import gettext as _
 from graphs.models import Graph
@@ -10,6 +9,7 @@ from graphs.models import Graph
 class Analytic(models.Model):
     graph = models.ForeignKey(Graph, related_name='analytics')
     #dump = models.ForeignKey(Dump, related_name='dump')
+    dump = models.CharField(_("dump"), max_length=250, null=True, blank=True)
 
     ALGOS = (
         ("pagerank", _("Page Rank")),
@@ -18,16 +18,17 @@ class Analytic(models.Model):
     )
 
     algorithm = models.CharField(max_length=8, choices=ALGOS)
-    #results = models.FileField()
+    results = models.CharField(_("results"), max_length=250, null=True, blank=True)
+    affected_nodes = models.CharField(_("affected_nodes"), max_length=250, null=True, blank=True)
 
     # tasks attributes
 
     task_id = models.CharField(_("task_id"), max_length=250, null=True, blank=True)
     task_start = models.DateTimeField(_("task_start"), null=True, blank=True)
     task_end = models.DateTimeField(_("task_end"), null=True, blank=True)
+    task_status = models.CharField(_("task_status"), max_length=250, null=True, blank=True)
 
     """
-    task_status
     task_errors
     task_message
     """
@@ -40,12 +41,16 @@ class AnalysisManager(models.Manager):
 
     def run(self, algorithm, **kwargs):
         analysis = self._graph.gdb.analysis()
-        algorithm_func = getattr(analysis, algorithm, None)
+        algorithm_func = getattr(analysis, algorithm)
+        # affected_nodes_array = "[669, 670, 671, 672, 673, 674, 675, 676, 677, 678, 679, 680, 693, 694, 716]"
+        # analytic = Analytic.objects.create(graph=self._graph, algorithm=algorithm, affected_nodes=affected_nodes_array)
         analytic = Analytic.objects.create(graph=self._graph, algorithm=algorithm)
         task = algorithm_func.apply_async(kwargs={'graph': self._graph, 'analytic': analytic})
         # Get the task values
-        analytic.task_start = datetime.datetime.now()
+        # while task.status != 'SUCCESS':
+        #     print task.status
         analytic.task_id = task.id
+        analytic.task_status = task.status
         analytic.save()
 
         return analytic
