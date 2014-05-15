8c44cfbe61fab2ce58d43e8af7051cdedc3730a1
gabmunrio <munozrios22@gmail.com>
2014-05-14 19:46:41 -0500

    New migration files for analytics including the data dump models.

--- sylva/analytics/migrations/0001_initial.py
+++ sylva/analytics/migrations/0001_initial.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import datetime
+from south.utils import datetime_utils as datetime
 from south.db import db
 from south.v2 import SchemaMigration
 from django.db import models
@@ -8,84 +8,112 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'DataDump'
+        db.create_table(u'analytics_datadump', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('creation_date', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('data_file', self.gf('django.db.models.fields.files.FileField')(max_length=100, null=True, blank=True)),
+        ))
+        db.send_create_signal(u'analytics', ['DataDump'])
+
         # Adding model 'Analytic'
-        db.create_table('analytics_analytic', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'analytics_analytic', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('graph', self.gf('django.db.models.fields.related.ForeignKey')(related_name='analytics', to=orm['graphs.Graph'])),
-            ('dump', self.gf('django.db.models.fields.CharField')(max_length=250, null=True, blank=True)),
-            ('algorithm', self.gf('django.db.models.fields.CharField')(max_length=8)),
-            ('results', self.gf('django.db.models.fields.CharField')(max_length=250, null=True, blank=True)),
-            ('task_id', self.gf('django.db.models.fields.CharField')(max_length=250, null=True, blank=True)),
+            ('dump', self.gf('django.db.models.fields.related.ForeignKey')(related_name='analytics', to=orm['analytics.DataDump'])),
+            ('algorithm', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('raw', self.gf('django.db.models.fields.files.FileField')(max_length=100, null=True, blank=True)),
+            ('results', self.gf('django.db.models.fields.files.FileField')(max_length=100, null=True, blank=True)),
+            ('subgraph', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('task_id', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
             ('task_start', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
             ('task_end', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+            ('task_status', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
+            ('task_error', self.gf('django.db.models.fields.CharField')(max_length=255, null=True, blank=True)),
         ))
-        db.send_create_signal('analytics', ['Analytic'])
+        db.send_create_signal(u'analytics', ['Analytic'])
 
 
     def backwards(self, orm):
+        # Deleting model 'DataDump'
+        db.delete_table(u'analytics_datadump')
+
         # Deleting model 'Analytic'
-        db.delete_table('analytics_analytic')
+        db.delete_table(u'analytics_analytic')
 
 
     models = {
-        'analytics.analytic': {
+        u'analytics.analytic': {
             'Meta': {'object_name': 'Analytic'},
-            'algorithm': ('django.db.models.fields.CharField', [], {'max_length': '8'}),
-            'dump': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
-            'graph': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'analytics'", 'to': "orm['graphs.Graph']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'results': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
+            'algorithm': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'dump': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'analytics'", 'to': u"orm['analytics.DataDump']"}),
+            'graph': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'analytics'", 'to': u"orm['graphs.Graph']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'raw': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'results': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            'subgraph': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'task_end': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
-            'task_id': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'}),
-            'task_start': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'})
+            'task_error': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'task_id': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
+            'task_start': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'task_status': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'})
         },
-        'auth.group': {
+        u'analytics.datadump': {
+            'Meta': {'object_name': 'DataDump'},
+            'creation_date': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'data_file': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        u'auth.group': {
             'Meta': {'object_name': 'Group'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
-            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
         },
-        'auth.permission': {
-            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
             'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         },
-        'auth.user': {
+        u'auth.user': {
             'Meta': {'object_name': 'User'},
             'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
-            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
-            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
         },
-        'contenttypes.contenttype': {
+        u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'data.data': {
+        u'data.data': {
             'Meta': {'object_name': 'Data'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'instance': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['engines.Instance']", 'null': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'instance': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['engines.Instance']", 'null': 'True', 'blank': 'True'}),
+            'last_modified_nodes': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'last_modified_relationships': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
             'options': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'total_analytics': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
             'total_nodes': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'total_queries': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'total_relationships': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
             'total_storage': ('django.db.models.fields.IntegerField', [], {'default': '0'})
         },
-        'engines.instance': {
+        u'engines.instance': {
             'Meta': {'object_name': 'Instance'},
             'activated': ('django.db.models.fields.NullBooleanField', [], {'default': 'True', 'null': 'True', 'blank': 'True'}),
             'activation': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
@@ -94,11 +122,11 @@ class Migration(SchemaMigration):
             'engine': ('django.db.models.fields.CharField', [], {'default': "'engines.gdb.backends.neo4j'", 'max_length': '50'}),
             'fragment': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'host': ('django.db.models.fields.CharField', [], {'default': "'localhost'", 'max_length': '250', 'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'key_file': ('django.db.models.fields.files.FileField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'options': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'instances'", 'to': "orm['auth.User']"}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'instances'", 'to': u"orm['auth.User']"}),
             'path': ('django.db.models.fields.CharField', [], {'default': "'db/data'", 'max_length': '250'}),
             'plain_password': ('django.db.models.fields.CharField', [], {'max_length': '50', 'null': 'True', 'blank': 'True'}),
             'port': ('django.db.models.fields.PositiveIntegerField', [], {'default': "'7474'", 'max_length': '10', 'null': 'True', 'blank': 'True'}),
@@ -107,23 +135,24 @@ class Migration(SchemaMigration):
             'type': ('django.db.models.fields.CharField', [], {'default': "'private'", 'max_length': '8', 'null': 'True', 'blank': 'True'}),
             'username': ('django.db.models.fields.CharField', [], {'max_length': '250', 'null': 'True', 'blank': 'True'})
         },
-        'graphs.graph': {
+        u'graphs.graph': {
             'Meta': {'ordering': "('order',)", 'unique_together': "(['owner', 'name'],)", 'object_name': 'Graph'},
-            'data': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['data.Data']", 'unique': 'True'}),
+            'data': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['data.Data']", 'unique': 'True'}),
             'description': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '120'}),
             'options': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
             'order': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'graphs'", 'to': "orm['auth.User']"}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'graphs'", 'to': u"orm['auth.User']"}),
             'public': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
             'relaxed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'schema': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['schemas.Schema']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'schema': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['schemas.Schema']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
             'slug': ('base.fields.AutoSlugField', [], {'db_index': 'False', 'unique': 'True', 'max_length': '200', 'populate_from': "['name']", 'blank': 'True'})
         },
-        'schemas.schema': {
+        u'schemas.schema': {
             'Meta': {'object_name': 'Schema'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'options': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
         }
     }
--- sylva/analytics/models.py
+++ sylva/analytics/models.py
@@ -1,32 +1,44 @@
 # -*- coding: utf-8 -*-
-
 from django.db import models
 
 from django.utils.translation import gettext as _
 from graphs.models import Graph
 
 
-def get_upload_to(self, filename):
-    return u"%s/%s/analytics/%s" % (self.graph.owner.username,
-                                    self.graph.slug, filename)
+def get_upload_to_analytics(self, filename):
+    return u"%s/analytics/%s" % (self.graph.slug, filename)
+
+
+def get_upload_to_dump(self, filename):
+    analytic = self.analytics.latest()
+    return u"%s/dumps/%s" % (analytic.graph.slug, filename)
+
+
+class DataDump(models.Model):
+    creation_date = models.DateTimeField(_("creation date"), null=True,
+                                         blank=True)
+    data_file = models.FileField(_("data file"), upload_to=get_upload_to_dump,
+                                 null=True, blank=True)
+
+    class Meta:
+        get_latest_by = "creation_date"
 
 
 class Analytic(models.Model):
-    graph = models.ForeignKey(Graph, related_name='analytics')
-    #dump = models.ForeignKey(Dump, related_name='dump')
-    dump = models.CharField(_("dump"), max_length=255, null=True,
-                            blank=True)
+    graph = models.ForeignKey(Graph, verbose_name=_("graph"),
+                              related_name='analytics')
+    dump = models.ForeignKey(DataDump, verbose_name=_("data dump"),
+                             related_name='analytics')
     algorithm = models.CharField(_("Algorithm"), max_length=255)
-    raw = models.FileField(_("Raw file"), upload_to=get_upload_to,
+    raw = models.FileField(_("Raw file"), upload_to=get_upload_to_analytics,
                            null=True, blank=True)
-    results = models.FileField(_("Results file"), upload_to=get_upload_to,
-                               null=True, blank=True)
+    results = models.FileField(_("Results file"), upload_to=
+                               get_upload_to_analytics, null=True, blank=True)
     subgraph = models.CharField(_("Subgraph"),
                                 max_length=255, null=True,
                                 blank=True)
 
     # tasks attributes
-
     task_id = models.CharField(_("task_id"), max_length=255, null=True,
                                blank=True)
     task_start = models.DateTimeField(_("task_start"), null=True,
@@ -37,6 +49,9 @@ class Analytic(models.Model):
                                    null=True, blank=True)
     task_error = models.CharField(_("task_error"), max_length=255,
                                   null=True, blank=True)
+
+    class Meta:
+        get_latest_by = "task_start"
 
 
 class AnalysisManager(models.Manager):
