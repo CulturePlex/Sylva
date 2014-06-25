50b72cf693e91cad533d49c0f57118395df9d251
gabmunrio <munozrios22@gmail.com>
2014-06-25 18:22:08 +0200

    First version of the model for the queries. We have added the initial migration too.

--- sylva/operators/models.py
+++ sylva/operators/models.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.utils.translation import gettext as _
+from jsonfield import JSONField
 
-# Create your models here.
+from graphs.models import Graph
+
+
+class Query(models.Model):
+    graph = models.ForeignKey(Graph, verbose_name=_("graph"),
+                              related_name='queries')
+    name = models.CharField(_("Name"), max_length=255)
+    description = models.CharField(_("Description"),
+                                   max_length=255)
+    results_count = models.IntegerField(_("Number of results"),
+                                        null=True, blank=True)
+    last_run = models.DateTimeField(_("Last time run"),
+                                    null=True, blank=True)
+    query_dict = JSONField()
+    query_aliases = JSONField()
+    query_fields = JSONField()
+
+    def __unicode__(self):
+      return self.name
