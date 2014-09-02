7c27652a3238d1053a5ddaadba1c4f54b66a5e04
davebshow <davebshow@gmail.com>
2014-09-01 23:45:47 -0400

    changed indent to spaces

--- sylva/reports/generator.py
+++ sylva/reports/generator.py
@@ -9,12 +9,12 @@ logger = get_task_logger(__name__)
 
 @app.task(name='reports.generate')
 def generate_report():
-	logger.info('Check Reports {0}'.format(datetime.datetime.now()))
-	templates = ReportTemplate.objects.all()
-	for template in templates:
-		if template.ready_to_execute():
-			logger.info('Ready to execute: {0} at {1}'.format(
-				template.name,
-				datetime.datetime.now()
-			))
-			template.execute()
+    logger.info('Check Reports {0}'.format(datetime.datetime.now()))
+    templates = ReportTemplate.objects.all()
+    for template in templates:
+        if template.ready_to_execute():
+            logger.info('Ready to execute: {0} at {1}'.format(
+                template.name,
+                datetime.datetime.now()
+            ))
+            template.execute()
