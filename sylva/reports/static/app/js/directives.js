0c1cdeafac65f6724fbe67972bcf32426240e519
davebshow <davebshow@gmail.com>
2014-10-10 14:33:26 -0400

    removed some logs

--- sylva/reports/static/app/js/directives.js
+++ sylva/reports/static/app/js/directives.js
@@ -639,24 +639,17 @@ directives.directive('sylvaEtCell', ['$sanitize', '$compile', 'DJANGO_URLS', fun
             }); 
 
             scope.$watch('activeX', function (newVal, oldVal) {
-                //if (newVal == oldVal) return;
-                console.log('newVal oldVal', newVal, oldVal)
                 if (newVal && newVal !== oldVal) {
-                    console.log('activeX')
                     var props = newVal.properties[0]
                     ,   init_dt = props.datatype;
-                    console.log('props', props)
                     if (init_dt !== 'number' && init_dt !== 'float' && init_dt !== 'auto_increment' && init_dt !== 'auto_increment_update' && 
                         props.aggregate === false) {
-                        console.log('IN', props.aggregate)
                         scope.ySeries = scope.ySeries.filter(function (el) {
                             var dt = el.properties[0].datatype
                             return dt === 'number' || dt === 'float' || dt === 'auto_increment' || dt === 'auto_increment_update' ||
                                 el.properties[0].aggregate !== false ; 
                         })
-                        console.log('ySeries', scope.ySeries)
                     } else {
-                        console.log('results', results)
                         scope.ySeries = results;
                         newVal = {alias: ''}
                     }
@@ -666,9 +659,6 @@ directives.directive('sylvaEtCell', ['$sanitize', '$compile', 'DJANGO_URLS', fun
             });
 
             scope.$watch('activeY', function (newVal, oldVal) {
-                console.log('new old', newVal, oldVal)
-                //if (newVal == oldVal) return;
-                console.log('activeY')
                 if (newVal && newVal !== oldVal) {
                     
                     var props = newVal.properties[0]
@@ -682,7 +672,6 @@ directives.directive('sylvaEtCell', ['$sanitize', '$compile', 'DJANGO_URLS', fun
                                 el.properties[0].aggregate !== false ; 
                         })
                     } else {
-                        console.log('else')
                         scope.xSeries = results;
                         newVal = {alias: ''};
                     }
