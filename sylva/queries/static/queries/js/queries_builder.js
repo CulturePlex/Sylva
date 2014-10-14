7a17f1d9f5bf57de8f47b7b983abe7ea0accdbd4
gabmunrio <munozrios22@gmail.com>
2014-10-14 12:34:16 +0200

    Query builder style changed like in the Schema view. Now, The form to save the query is a collapsible div.

--- sylva/queries/static/queries/js/queries_builder.js
+++ sylva/queries/static/queries/js/queries_builder.js
@@ -1,8 +1,30 @@
 (function ($) {
+    var menuOpened = null;
+    var accordionState = "disabled";
 
-  $('.body-inside').css({
-    "width": "570px"
-  });
+    // A function that opens a menu depending of the selector of the button.
+    var openMenu = function(selector) {
+      closeMenu();
+      menuOpened = $(selector);
+
+      var angle = menuOpened.siblings().children()[1];
+      $(angle).removeClass('fa-angle-down');
+      $(angle).addClass('fa-angle-up');
+
+      menuOpened.show();
+    };
+
+    // A function that checks if a menu is opened and closes it.
+    var closeMenu = function() {
+      if (menuOpened != null) {
+        var angle = menuOpened.siblings().children()[1];
+        $(angle).removeClass('fa-angle-up');
+        $(angle).addClass('fa-angle-down');
+
+        menuOpened.hide();
+        menuOpened = null;
+      }
+    };
 
   $('#builder-query').on('click', function() {
     $('#query-builder-results').hide();
@@ -16,21 +38,78 @@
     $('#results').show();
   });
 
-  $('#select-node').on('mouseover', function() {
-    $('#node-options').css({
-      "display": "block"
-    });
+  $('#select-node-type').on("mouseover", function() {
+      openMenu("#node-types");
   });
 
-  $('#select-node').on('mouseout', function() {
-    $('#node-options').css({
-      "display": "none"
-    });
-  });
+  $("#node-types").on("mouseleave", closeMenu);
+
 
   $('#clear-button').on('click', function() {
     jsPlumb.detachEveryConnection();
     $('#diagram').empty();
+    // We remove the endpoints in the previous level (because JsPlumb update)
+    elems = $('#diagramContainer').children();
+    $.each(elems, function(index, elem) {
+        var elemClass = $(elem).attr('class');
+        if(elemClass != undefined) {
+            var filter = new RegExp("_jsPlumb.");
+            if(elemClass.match(filter)) {
+              $(elem).remove();
+            }
+        }
+    });
+  });
+
+
+  $('.accordion-save-query').accordion({
+    collapsible: true,
+    create: function(event, ui) {
+      var box = $(event.target);
+      var children = box.children();
+      var header =  children.first();
+      var body = $(children[1]);
+      var span = header.children().first();
+
+      // The next lines remove jQueryUI style from the boxes.
+      box.removeClass('ui-widget ui-accordion');
+      header.removeClass('ui-accordion-icons ' +
+        'ui-accordion-header ui-helper-reset ui-state-default');
+      body.removeClass('ui-accordion-content ui-widget-content');
+      body.css('height', '');
+      span.remove();
+    },
+    activate: function(event) {
+      if(accordionState == "disabled") {
+        $('.content-divider').css({
+          "display": ""
+        });
+        $('.content2-second').css({
+          "float": "left",
+          "width": "740px",
+          "padding-left": "30px"
+        });
+        $('#diagramContainer').css({
+          "margin-top": "32px"
+        });
+        accordionState = "enabled";
+      } else if(accordionState == "enabled") {
+        $('.content-divider').css({
+          "display": "none"
+        });
+        $('.content2-second').css({
+          "float": "none",
+          "width": "1140px",
+          "padding-left": "0px"
+        });
+        $('#diagramContainer').css({
+          "margin-top": "0px"
+        });
+        accordionState = "disabled";
+      }
+    },
+    active: false,
+    heightStyle: 'content'
   });
 
 })(jQuery);
