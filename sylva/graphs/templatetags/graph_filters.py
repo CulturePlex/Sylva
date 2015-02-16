3a082d84aba5f4c5d93d8b5cecbdf7f2af78c3a9
Javier de la Rosa <versae@gmail.com>
2015-02-16 17:12:31 -0500

    Fix the (Empty) problem in node type listings

--- sylva/graphs/templatetags/graph_filters.py
+++ sylva/graphs/templatetags/graph_filters.py
@@ -8,6 +8,7 @@ register = template.Library()
 
 @register.filter
 def get(value, key, default=None):
+    default = u"" if default is None else default
     return unicode(value.get(key, default))
 
 
@@ -17,6 +18,7 @@ def list_analytics(graph, algorithm):
         algorithm=algorithm,
         dump__graph=graph).order_by('-task_start')[:10]
     return l
+
 
 @register.filter
 def get_iso_format(analytic):
