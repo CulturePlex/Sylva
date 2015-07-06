3267bcc2ac2bdf0857c9169175528056c1a5d3b0
davebshow <davebshow@gmail.com>
2015-07-06 15:54:44 -0400

    added new constant to fixtures, should fix broken build

--- sylva/reports/fixtures/mock_urls.js
+++ sylva/reports/fixtures/mock_urls.js
@@ -1,14 +1,15 @@
 var djangoUrls = {
     builder: "/reports/dh/builder",
-                collabs: "/data/dh/collaborators/lookup/",
-                del: "/reports/dh/delete",
-                history: "/reports/dh/history",
-                list: "/reports/dh/list",
-                partials: "/reports/dh/partials",
-                preview: "/reports/dh/pdf/preview",
-                templates: "/reports/dh/templates"
-            }
-
+    collabs: "/data/dh/collaborators/lookup/",
+    del: "/reports/dh/delete",
+    history: "/reports/dh/history",
+    list: "/reports/dh/list",
+    partials: "/reports/dh/partials",
+    preview: "/reports/dh/pdf/preview",
+    templates: "/reports/dh/templates"
+}
+var perms ={view: true, add: true, edit: true, del: true};
+angular.module('reports').constant("REPORTS_PERMS", perms);
 angular.module('reports').constant("DJANGO_URLS", djangoUrls);
 angular.module('reports').constant("GRAPH", "dh");
 angular.module('reports').constant("AS_MODAL", "");
