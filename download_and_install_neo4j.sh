45ce6bcb41a9ce1ac22003cb14c82b0a44d49a8b
Javier de la Rosa <versae@gmail.com>
2015-07-09 13:53:56 -0400

    Add Travis cache and dockerized execution

--- download_and_install_neo4j.sh
+++ download_and_install_neo4j.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-DEFAULT_VERSION="1.9.7"
+DEFAULT_VERSION="1.9.9"
 VERSION=${1-$DEFAULT_VERSION}
 OLD_DIR="neo4j-community-$VERSION"
 NEW_DIR="neo4j"
 FILE="$OLD_DIR-unix.tar.gz"
 DOWNLOAD_SERVER="http://dist.neo4j.org/"
 DOWNLOAD_URL="$DOWNLOAD_SERVER$FILE"
-SERVER_PROPERTIES_FILE="neo4j/conf/neo4j-server.properties"
-OLD_URL="\/db\/data\/"
-NEW_URL="\/db\/sylva\/"
-OLD_PORT="7474"
-NEW_PORT="7373"
 
-wget $DOWNLOAD_URL
-tar -zxvf $FILE > /dev/null
-rm $FILE
-mv $OLD_DIR $NEW_DIR
+if [ ! -d "$NEW_DIR" ]; then
+  wget $DOWNLOAD_URL
+  tar -zxvf $FILE > /dev/null
+  rm $FILE
+  mv $OLD_DIR $NEW_DIR
 
-if grep $OLD_PORT $SERVER_PROPERTIES_FILE > /dev/null; then
-    sed -i s/$OLD_PORT/$NEW_PORT/g  $SERVER_PROPERTIES_FILE
-fi
+  SERVER_PROPERTIES_FILE="neo4j/conf/neo4j-server.properties"
+  OLD_URL="\/db\/data\/"
+  NEW_URL="\/db\/sylva\/"
+  OLD_PORT="7474"
+  NEW_PORT="7373"
 
-if grep $OLD_URL $SERVER_PROPERTIES_FILE > /dev/null; then
-    sed -i s/$OLD_URL/$NEW_URL/g  $SERVER_PROPERTIES_FILE
+  if grep $OLD_PORT $SERVER_PROPERTIES_FILE > /dev/null; then
+      sed -i s/$OLD_PORT/$NEW_PORT/g  $SERVER_PROPERTIES_FILE
+  fi
+
+  if grep $OLD_URL $SERVER_PROPERTIES_FILE > /dev/null; then
+      sed -i s/$OLD_URL/$NEW_URL/g  $SERVER_PROPERTIES_FILE
+  fi
+else
+  echo "Using cached directory '$NEW_DIR'."
+  rm -rf "$NEW_DIR/data/"
+  mkdir "$NEW_DIR/data"
+  mkdir "$NEW_DIR/data/log"
 fi
